ccc751ab9f914676b94a2543aab8b15c556767c3
current <161969964+current-meantime@users.noreply.github.com>
2025-05-02 17:05:28 +0200

    create lcd.sh

---
+++ lcd.sh
@@ -0,0 +1,7 @@
+# essentially, 'ls' but lines are numbered, takes in normal 'ls' options
+# usage: lcd ['ls' options] [path]
+# suggested usage: in combination with 'cds' and 'sel' utils
+
+lcd() {
+  ls -1 "$@" | nl
+}
