39774e5aeb87d118115f201e3863a40978a603d8
current <161969964+current-meantime@users.noreply.github.com>
2025-05-02 17:09:39 +0200

    create cds.sh

---
+++ cds.sh
@@ -0,0 +1,12 @@
+# 'cd' to 'selected' = 'cds'
+# usage: cds [dir number]
+# you can get the dir number by running 'lcd' if you don't know it
+
+cds() {
+  target=$(ls -1 | sed -n "${1}p")
+  if [ -d "$target" ]; then
+    cd "$target"
+  else
+    echo "No directory found with number: $1"
+  fi
+}
